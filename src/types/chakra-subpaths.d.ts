2d50de9b7108f4b1fe55f08bea6c6abec67edd89
Kiran Parmar <kiran.parmar@digitalfactory24.com>
2025-11-26 14:17:46 +0530

    package add

---
+++ src/types/chakra-subpaths.d.ts
@@ -0,0 +1,193 @@
+declare module "@chakra-ui/form-control";
+declare module "@chakra-ui/checkbox";
+declare module "@chakra-ui/some-other-subpath";
+declare module "@chakra-ui/another-subpath";
+declare module "@chakra-ui/yet-another-subpath";
+declare module "@chakra-ui/react/*" {
+  import { ChakraProps } from "@chakra-ui/react";
+  export = ChakraProps;
+}
+declare module "@chakra-ui/react" {
+  import * as Chakra from "@chakra-ui/react/index";
+  export = Chakra;
+}
+declare module "@chakra-ui/react/index" {
+  import * as Chakra from "@chakra-ui/react/index";
+  export = Chakra;
+}   
+declare module "@chakra-ui/react/utils/*" {
+  import { ChakraProps } from "@chakra-ui/react";
+  export = ChakraProps;
+}
+declare module "@chakra-ui/react/form-control" {
+  import { ChakraProps } from "@chakra-ui/react";
+  export = ChakraProps;
+}
+declare module "@chakra-ui/react/checkbox" {
+  import { ChakraProps } from "@chakra-ui/react";
+  export = ChakraProps;
+}
+declare module "@chakra-ui/react/some-other-subpath" {
+  import { ChakraProps } from "@chakra-ui/react";
+  export = ChakraProps;
+}
+declare module "@chakra-ui/react/another-subpath" {
+  import { ChakraProps } from "@chakra-ui/react";
+  export = ChakraProps;
+}
+declare module "@chakra-ui/react/yet-another-subpath" {
+  import { ChakraProps } from "@chakra-ui/react";
+  export = ChakraProps;
+}
+declare module "@chakra-ui/react/utils" {
+  import { ChakraProps } from "@chakra-ui/react";
+  export = ChakraProps;
+}
+declare module "@chakra-ui/react/form-control/index" {
+  import { ChakraProps } from "@chakra-ui/react";
+  export = ChakraProps;
+}
+declare module "@chakra-ui/react/checkbox/index" {
+  import { ChakraProps } from "@chakra-ui/react";
+  export = ChakraProps;
+}
+declare module "@chakra-ui/react/some-other-subpath/index" {
+  import { ChakraProps } from "@chakra-ui/react";
+  export = ChakraProps;
+}
+declare module "@chakra-ui/react/another-subpath/index" {
+  import { ChakraProps } from "@chakra-ui/react";
+  export = ChakraProps;
+}
+declare module "@chakra-ui/react/yet-another-subpath/index" {
+  import { ChakraProps } from "@chakra-ui/react";
+  export = ChakraProps;
+}
+declare module "@chakra-ui/react/utils/index" {
+  import { ChakraProps } from "@chakra-ui/react";
+  export = ChakraProps;
+}
+declare module "@chakra-ui/react/form-control/utils" {
+  import { ChakraProps } from "@chakra-ui/react";
+  export = ChakraProps;
+}
+declare module "@chakra-ui/react/checkbox/utils" {
+  import { ChakraProps } from "@chakra-ui/react";
+  export = ChakraProps;
+}
+declare module "@chakra-ui/react/some-other-subpath/utils" {
+  import { ChakraProps } from "@chakra-ui/react";
+  export = ChakraProps;
+}
+declare module "@chakra-ui/react/another-subpath/utils" {
+  import { ChakraProps } from "@chakra-ui/react";
+  export = ChakraProps;
+}
+declare module "@chakra-ui/react/yet-another-subpath/utils" {
+  import { ChakraProps } from "@chakra-ui/react";
+  export = ChakraProps;
+}   
+declare module "@chakra-ui/react/form-control/form-control" {
+  import { ChakraProps } from "@chakra-ui/react";
+  export = ChakraProps;
+}
+declare module "@chakra-ui/react/checkbox/checkbox" {
+  import { ChakraProps } from "@chakra-ui/react";
+  export = ChakraProps;
+}
+declare module "@chakra-ui/react/some-other-subpath/some-other-subpath" {   
+  import { ChakraProps } from "@chakra-ui/react";
+  export = ChakraProps;
+}
+declare module "@chakra-ui/react/another-subpath/another-subpath" {
+  import { ChakraProps } from "@chakra-ui/react";
+  export = ChakraProps;
+}
+declare module "@chakra-ui/react/yet-another-subpath/yet-another-subpath" {
+  import { ChakraProps } from "@chakra-ui/react";
+  export = ChakraProps;
+}
+declare module "@chakra-ui/react/utils/utils" {
+  import { ChakraProps } from "@chakra-ui/react";
+  export = ChakraProps;
+}       
+declare module "@chakra-ui/react/form-control/form-control/index" {
+  import { ChakraProps } from "@chakra-ui/react";
+  export = ChakraProps;
+}
+declare module "@chakra-ui/react/checkbox/checkbox/index" {
+    import { ChakraProps } from "@chakra-ui/react";     
+    export = ChakraProps;   
+}
+declare module "@chakra-ui/react/some-other-subpath/some-other-subpath/index" { 
+  import { ChakraProps } from "@chakra-ui/react";
+  export = ChakraProps;
+}
+declare module "@chakra-ui/react/another-subpath/another-subpath/index" {
+  import { ChakraProps } from "@chakra-ui/react";
+  export = ChakraProps;
+}
+declare module "@chakra-ui/react/yet-another-subpath/yet-another-subpath/index" {
+  import { ChakraProps } from "@chakra-ui/react";
+  export = ChakraProps;
+}
+declare module "@chakra-ui/react/utils/utils/index" {
+  import { ChakraProps } from "@chakra-ui/react";
+  export = ChakraProps;
+}
+declare module "@chakra-ui/react/form-control/form-control/utils" {
+  import { ChakraProps } from "@chakra-ui/react";
+  export = ChakraProps;
+}
+declare module "@chakra-ui/react/checkbox/checkbox/utils" { 
+  import { ChakraProps } from "@chakra-ui/react";   
+    export = ChakraProps;       
+}
+declare module "@chakra-ui/react/some-other-subpath/some-other-subpath/utils" {
+  import { ChakraProps } from "@chakra-ui/react";
+  export = ChakraProps;
+}
+declare module "@chakra-ui/react/another-subpath/another-subpath/utils" {
+  import { ChakraProps } from "@chakra-ui/react";
+  export = ChakraProps;
+}
+declare module "@chakra-ui/react/yet-another-subpath/yet-another-subpath/utils" {
+  import { ChakraProps } from "@chakra-ui/react";
+  export = ChakraProps;
+}   
+declare module "@chakra-ui/react/utils/utils/utils" {
+  import { ChakraProps } from "@chakra-ui/react";
+  export = ChakraProps;
+}   
+declare module "@chakra-ui/react/form-control/form-control/form-control" {
+  import { ChakraProps } from "@chakra-ui/react";
+  export = ChakraProps;
+}   
+declare module "@chakra-ui/react/checkbox/checkbox/checkbox" {
+  import { ChakraProps } from "@chakra-ui/react";
+  export = ChakraProps;
+}   
+declare module "@chakra-ui/react/some-other-subpath/some-other-subpath/some-other-subpath" {
+  import { ChakraProps } from "@chakra-ui/react";
+  export = ChakraProps;
+}
+declare module "@chakra-ui/react/another-subpath/another-subpath/another-subpath" {
+  import { ChakraProps } from "@chakra-ui/react";
+  export = ChakraProps;
+}   
+declare module "@chakra-ui/react/yet-another-subpath/yet-another-subpath/yet-another-subpath" {
+  import { ChakraProps } from "@chakra-ui/react";
+  export = ChakraProps;
+}
+declare module "@chakra-ui/react/utils/utils/utils" {
+  import { ChakraProps } from "@chakra-ui/react";
+  export = ChakraProps;
+}
+declare module "@chakra-ui/react/form-control/form-control/form-control/index" {        
+  import { ChakraProps } from "@chakra-ui/react";
+  export = ChakraProps;
+}
+declare module "@chakra-ui/react/checkbox/checkbox/checkbox/index" {
+  import { ChakraProps } from "@chakra-ui/react";
+  export = ChakraProps;
+}
